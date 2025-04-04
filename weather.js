8c52c467ddb2b55c8683914ccee33d4189faf2e2
Riaan Harwood <104803762+riaanHarwood@users.noreply.github.com>
2025-04-04 18:26:07 +1300

    Developed and integrated the weather API's
    Integrated weather apis from openweathermap website
    
    Bugs to be worked on: images not generating when searching for a city and or country

--- weather.js
+++ weather.js
@@ -1,15 +1,82 @@
 const cityInput = document.querySelector('.city-input')
 const searchBtn = document.querySelector('.search-btn')
 
+const weatherInfoSection = document.querySelector('.weather-info')
+const notFoundSection = document.querySelector('.not-found')
+const searchCitySection = document.querySelector('.search-city')
+
+const countryTxt = document.querySelector('.country\\.txt')
+const tempTxt = document.querySelector('.temp-txt')
+const conditionTxt = document.querySelector('.condition-txt')
+const humidityValueTxt = document.querySelector('.humidity-value-txt')
+const windValueTxt = document.querySelector('.wind-value-txt')
+const weatherSummaryImg = document.querySelector('.weather-summary-img')
+const currentDateTxt = document.querySelector('.current-date.txt')
+
+const apiKey = '6d4f5538fdd126f22b975ca85bf6d255'
+
+
 searchBtn.addEventListener('click', () => {
-    if (cityInput.value.trim() != '' ){
-        console.log(cityInput.value)
+    if (cityInput.value.trim() != ''){
+        updateWeatherInfo(cityInput.value)
         cityInput.value= ''
-        cityInput.blur() 
+        cityInput.blur()
     }
 
 })
 
-cityInput.addEventListener('keydown', (event) =>{
-    console.log(event)
-})
+cityInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter' && cityInput.value.trim() !== '') {
+        updateWeatherInfo(cityInput.value);
+        cityInput.value = '';
+        cityInput.blur();
+    }
+});
+
+
+async function getFetchData(endPoint, city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/${endPoint}?q=${city}&appid=${apiKey}&units=metric`;
+    const response = await fetch(apiUrl);
+    return response.json();
+}
+
+
+
+function getWeatherIcon(id){
+    console.log(id)
+}
+
+// add to this function --> .cod///
+async function updateWeatherInfo(city) {
+    const weatherData = await getFetchData('weather', city)
+
+    if (weatherData.cod != 200){
+        showDisplaySection(notFoundSection)
+        return 
+    }
+    console.log(weatherData)
+
+    const {
+        name: country, 
+        main: {temp, humidity}, 
+        weather: [{ id, main, }],
+        wind: { speed }
+    } = weatherData
+
+    countryTxt.textContent = country
+    tempTxt.textContent = Math.round(temp) + " °C"
+    conditionTxt.textContent = main
+    humidityValueTxt.textContent = humidity + "%"
+    windValueTxt.textContent = speed + " M/s"
+    weatherSummaryImg.src = `./assets/weather/${getWeatherIcon(id)}`
+
+    showDisplaySection(weatherInfoSection)
+
+}
+
+function showDisplaySection(section) {
+    [weatherInfoSection, searchCitySection, notFoundSection]
+        .forEach(section => section.style.display='none')
+
+    section.style.display='flex'
+}
